844b2c9352366772a25fc7483c628743a4fa0a0c
Jisse Reitsma <info@yireo.com>
2014-05-27 12:47:07 +0200

    Add new experimental JS compression using methods of Jeroen

--- source/components/com_scriptmerge/helpers/helper.php
+++ source/components/com_scriptmerge/helpers/helper.php
@@ -40,18 +40,18 @@ class ScriptMergeHelper
 
         // If compression is enabled
         $application = JFactory::getApplication();
-        $compress_js = ScriptMergeHelper::getParams()->get('compress_js', 0);
-        if ($application->isSite() && $compress_js > 0) {
+        $compress_js = ScriptMergeHelper::getParams()->get('compress_js');
+        if ($application->isSite() && !empty($compress_js)) {
 
             // JsMinPlus definitely does not work with MooTools (for now)
-            if($compress_js == 2 && stristr($file, 'mootools') == true) {
-                $compress_js = 1;
+            if($compress_js == 'jsminplus' && stristr($file, 'mootools') == true) {
+                $compress_js = 'simple';
             }
 
             // Switch between the various compression-schemes
             switch ($compress_js) {
 
-                case 1:
+                case 'simple':
                     $buffer = str_replace('/// ', '///', $buffer);		
 				    $buffer = str_replace(',//', ', //', $buffer);
     				$buffer = str_replace('{//', '{ //', $buffer);
@@ -78,7 +78,7 @@ class ScriptMergeHelper
                     $buffer = preg_replace('/\s+/', ' ', $buffer);
                     break;
 
-                case 2:
+                case 'jsminplus':
                     // Compress the js-code
                     $jsMinPhp = JPATH_SITE.'/components/com_scriptmerge/lib/jsminplus.php';
                     if(file_exists($jsMinPhp)) {
@@ -89,7 +89,7 @@ class ScriptMergeHelper
                     }
                     break;
 
-                case 3:
+                case 'closurecompiler':
                     // Compress the js-code through the Google Closure Compiler API
                     $url = 'http://closure-compiler.appspot.com/compile';
 
@@ -122,6 +122,61 @@ class ScriptMergeHelper
                             $buffer = $json['compiledCode'];
                         }
                     }
+                    break;
+
+                case 'experimental':
+                    //START Remove comments.
+                    $buffer = str_replace('/// ', '///', $buffer);      
+                    $buffer = str_replace(',//', ', //', $buffer);
+                    $buffer = str_replace('{//', '{ //', $buffer);
+                    $buffer = str_replace('}//', '} //', $buffer);
+                    $buffer = str_replace('*//*', '*/  /*', $buffer);
+                    $buffer = str_replace('/**/', '/*  */', $buffer);
+                    $buffer = str_replace('*///', '*/ //', $buffer);
+                    $buffer = preg_replace("/\/\/.*\n\/\/.*\n/", "", $buffer);
+                    $buffer = preg_replace("/\s\/\/\".*/", "", $buffer);
+                    $buffer = preg_replace("/\/\/\n/", "\n", $buffer);
+                    $buffer = preg_replace("/\/\/\s.*.\n/", "\n  \n", $buffer);
+                    $buffer = preg_replace('/\/\/w[^w].*/', '', $buffer);
+                    $buffer = preg_replace('/\/\/s[^s].*/', '', $buffer);
+                    $buffer = preg_replace('/\/\/\*\*\*.*/', '', $buffer);
+                    $buffer = preg_replace('/\/\/\*\s\*\s\*.*/', '', $buffer);
+                    $buffer = preg_replace('/([^\":\-1\\\.Cb\n])(\/\/)([^,"\';$)ws8E].*)/', '$1$2xxxpp$3', $buffer);
+                    do {$buffer = preg_replace('/((\(\s*|\=\s*)[\"\'\\\\]\s*)(([^\(\);]*)xxxpp)/','$1$4', $buffer, 1, $count);} while ($count);
+                    $buffer = preg_replace('/([^=:\-1\\\.Cb\n])(\/\/)xxxpp([^,"\';$)ws8].*)/', '$1', $buffer);
+                    $buffer = preg_replace("/([^\*])[\/]+\/\*.*[^a-zA-Z0-9\s\-=+\|!@#$%^&()`~\[\]{};:\'\",<.>?]/", "$1", $buffer);
+                    $buffer = preg_replace("/\/\*/", "\n/*dddpp", $buffer);
+                    $buffer = preg_replace('/((\{\s*|:\s*)[\"\']\s*)(([^\{\};\"\']*)dddpp)/','$1$4', $buffer);
+                    $buffer = preg_replace("/\*\//", "xxxpp*/\n", $buffer);
+                    $buffer = preg_replace('/((\{\s*|:\s*|\[\s*)[\"\']\s*)(([^\};\"\']*)xxxpp)/','$1$4', $buffer);
+                    $buffer = preg_replace('/([\"\'])\s*\/\*/', '$1/*', $buffer);
+                    $buffer = preg_replace('/(\n)[^\'"]?\/\*dddpp.*?xxxpp\*\//s', '', $buffer);
+                    $buffer = preg_replace('/\n\/\*dddpp([^\s]*)/', '$1', $buffer);
+                    $buffer = preg_replace('/xxxpp\*\/\n([^\s]*)/', '*/$1', $buffer);
+                    $buffer = preg_replace('/xxxpp\*\/\n([\"])/', '$1', $buffer);
+                    $buffer = preg_replace('/(\*)\n*\s*(\/\*)\s*/', '$1$2$3', $buffer);
+                    $buffer = preg_replace('/(\*\/)\s*(\")/', '$1$2', $buffer);
+                    $buffer = preg_replace('/\/\*dddpp(\s*)/', '/*', $buffer);
+                    $buffer = preg_replace('/\n\s*\n/', "\n", $buffer);
+                    $buffer = preg_replace("/(<!--.*-->)/Us","$1QQXQQ", $buffer);
+                    do {$buffer = preg_replace("/(\(\s*[\"]\s*[^\(\)\"]*<!--.*-->)QQXQQ/Us","$1", $buffer, 1, $count);} while ($count);
+                    do {$buffer = preg_replace("/(\(\s*[\']\s*[^\(\)\']*<!--.*-->)QQXQQ/Us","$1", $buffer, 1, $count);} while ($count);
+                    $buffer = preg_replace("/(<!--.*-->QQXQQ)/Us","", $buffer);
+                    $buffer = preg_replace('/([^\"=:\-1\\\.Cb]\/\/)([^,"*\';$)ws8E].*)/', '$1xpxpp$2', $buffer);
+                    do {$buffer = preg_replace('/([^\"\'])(([\(]|[\:]|[\=]|[\+])\s*[\"]\s*)(?!([\'\+]))([^\"\\\\,]*)(xpxpp)/', '$1$2$5', $buffer, 1, $count);} while ($count);
+                    do {$buffer = preg_replace('/([^\'])(([\(]|[\:]|[\=]|[\+])\s*[\']\s*)(?!([\"\+]))([^\'\\\\,]*)(xpxpp)/', '$1$2$5', $buffer, 1, $count);} while ($count);
+                    do {$buffer = preg_replace('/((\(\s*|\=\s*)[\"\'\\\\]\s*)(([^\(\);,]*)xpxpp)/','$1$4', $buffer, 1, $count);} while ($count);
+                    $buffer = preg_replace('/([^=:\-1\\\.Cb])(\/\/)xpxpp([^,"*\';$)ws8].*)/', '$1', $buffer);
+                    $buffer = preg_replace('/(^\/\/[^,"*\';$)ws8].*)/', '', $buffer);
+                    $buffer = preg_replace('/([^\n\w\-=+\|!@#$%^&*()`~\[\]{};:\'",<.>\/?\\\\])(\/\/)(.*)/', '$1', $buffer);
+                    $buffer = preg_replace('/((\R)|([^0]1)|([^\"]\-))(\/\/)(.*)/', '$1', $buffer);
+                    //END Remove comments.  
+                    //START Remove all whitespaces
+                    $buffer = preg_replace('/\s+/', ' ', $buffer);
+                    $buffer = preg_replace('/\s*(?:(?=[=\-\+\|%&\*\)\[\]\{\};:\,\.\<\>\!\@\#\^`~]))/', '', $buffer);
+                    $buffer = preg_replace('/(?:(?<=[=\-\+\|%&\*\)\[\]\{\};:\,\.\<\>\?\!\@\#\^`~]))\s*/', '', $buffer);
+                    $buffer = preg_replace('/([^a-zA-Z0-9\s\-=+\|!@#$%^&*()`~\[\]{};:\'",<.>\/?])\s+([^a-zA-Z0-9\s\-=+\|!@#$%^&*()`~\[\]{};:\'",<.>\/?])/', '$1$2', $buffer);
+                    //END Remove all whitespaces
                     break;
 
                 case 0:
