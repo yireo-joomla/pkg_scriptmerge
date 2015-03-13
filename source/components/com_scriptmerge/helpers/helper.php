4f458b494229437da5df42460c199ff6875fe39a
Jisse Reitsma <info@yireo.com>
2015-03-13 09:35:42 +0100

    Add new JS compression by js3016437 as option "simple2"

--- source/components/com_scriptmerge/helpers/helper.php
+++ source/components/com_scriptmerge/helpers/helper.php
@@ -76,6 +76,53 @@ class ScriptMergeHelper
     				$buffer = preg_replace("/<!--.*-->/Us","", $buffer);
                     break;
 
+                case 'simple2':
+                    $buffer = str_replace('/// ', '///', $buffer);      
+                    $buffer = str_replace(',//', ', //', $buffer);
+                    $buffer = str_replace('{//', '{ //', $buffer);
+                    $buffer = str_replace('}//', '} //', $buffer);
+                    $buffer = str_replace('*//*', '*/  /*', $buffer);
+                    $buffer = str_replace('/**/', '/*  */', $buffer);
+                    $buffer = str_replace("*/", "\n */", $buffer);
+                    $buffer = str_replace('*///', '*/ //', $buffer);
+                    $buffer = preg_replace("/([0-9]\s*)(\/\/)(\s*[0-9]+\s*\")/", "$1/dxzdxz/$3dxzdxz", $buffer);
+                    $buffer = preg_replace("/\/\/.*\n\/\/.*\n/", "", $buffer);
+                    $buffer = preg_replace("/\s\/\/\".*/", "", $buffer);
+                    $buffer = preg_replace("/\/\/\n/", "\n", $buffer);
+                    $buffer = preg_replace("/\/\/\s[a-zA-Z0-9\-=+\|!@#$%^&()`~\[\]{};:\'\",<.>?]*[\n\r]/", "\n  \n", $buffer);
+                    $buffer = preg_replace('/\/\/w[^w].*/', '', $buffer);
+                    $buffer = preg_replace('/\/\/s[^s].*/', '', $buffer);
+                    $buffer = preg_replace('/\/\/\*\*\*.*/', '', $buffer);
+                    $buffer = preg_replace('/\/\/\*\s\*\s\*.*/', '', $buffer);
+                    $buffer = preg_replace('/[^\*]\/\/[*].*/', '', $buffer);
+                    $buffer = preg_replace('/([;])\/\/.*/', '$1', $buffer);
+                    $buffer = preg_replace('/((\r)|(\n)|(\R)|([^0]1)|([^\"]\s*\-))(\/\/)(.*)/', '$1', $buffer);
+                    $buffer = preg_replace("/([^\*])[\/]+\/\*.*[^a-zA-Z0-9\s\-=+\|!@#$%^&()`~\[\]{};:\'\",<.>?]/", "$1", $buffer);
+                    $buffer = preg_replace("/\/\*/", "\n/*dddpp", $buffer);
+                    $buffer = preg_replace('/((\{\s*|:\s*)[\"\']\s*)(([^\{\};\"\']*)dddpp)/','$1$4', $buffer);
+                    $buffer = preg_replace("/\*\//", "xxxpp*/\n", $buffer);
+                    $buffer = preg_replace('/((\{\s*|:\s*|\[\s*)[\"\']\s*)(([^\};\"\']*)xxxpp)/','$1$4', $buffer);
+                    $buffer = preg_replace('/([\"\'])\s*\/\*/', '$1/*', $buffer);
+                    $buffer = preg_replace('/(\n)[^\'"]?\/\*dddpp.*?xxxpp\*\//s', '', $buffer);
+                    $buffer = preg_replace('/\n\/\*dddpp([^\s]*)/', '$1', $buffer);
+                    $buffer = preg_replace('/xxxpp\*\/\n([^\s]*)/', '*/$1', $buffer);
+                    $buffer = preg_replace('/xxxpp\*\/\n([\"])/', '$1', $buffer);
+                    $buffer = preg_replace('/(\*)\n*\s*(\/\*)\s*/', '$1$2$3', $buffer);
+                    $buffer = preg_replace('/(\*\/)\s*(\")/', '$1$2', $buffer);
+                    $buffer = preg_replace('/\/\*dddpp(\s*)/', '/*', $buffer);
+                    $buffer = preg_replace('/\n\s*\n/', "\n", $buffer);
+                    $buffer = preg_replace("/([^\'\"]\s*)<!--.*-->(?!(<\/div>)).*/","$1", $buffer);
+                    $buffer = preg_replace('/([^\n\w\-=+\|!@#$%^&*()`~\[\]{};:\'",<.>\/?\\\\])(\/\/)(.*)/', '$1', $buffer);
+                    $buffer = preg_replace("/\/\/\s.*[\n|\r]/", "\n  \n", $buffer); 
+                    $buffer = preg_replace('/dxzdxz/', '', $buffer);
+                    $buffer = preg_replace('/\s+(\*\/)/', '$1', $buffer);
+
+                    $buffer = preg_replace('/\s+/', ' ', $buffer);
+                    $buffer = preg_replace('/\s*(?:(?=[=\-\+\|%&\*\)\[\]\{\};:\,\.\<\>\!\@\#\^`~]))/', '', $buffer);
+                    $buffer = preg_replace('/(?:(?<=[=\-\+\|%&\*\)\[\]\{\};:\,\.\<\>\?\!\@\#\^`~]))\s*/', '', $buffer);
+                    $buffer = preg_replace('/([^a-zA-Z0-9\s\-=+\|!@#$%^&*()`~\[\]{};:\'",<.>\/?])\s+([^a-zA-Z0-9\s\-=+\|!@#$%^&*()`~\[\]{};:\'",<.>\/?])/', '$1$2', $buffer);
+                    break;
+
                 case 'jsminplus':
                     // Compress the js-code
                     $jsMinPhp = JPATH_SITE.'/components/com_scriptmerge/lib/jsminplus.php';
